28f2dfe1bbb5ac058c33b8040975fb1c2d4859fc
Muhammed Zeba <parice02@hotmail.com>
2022-08-06 22:49:26 +0000

    update

--- cli.py
+++ cli.py
@@ -1,18 +1,31 @@
-if __name__ == "__main__":
-    from pprint import pprint
+from pprint import pprint
 
-    from anagram import Anagram
+from anagram import Anagram
 
+
+def main(letters: str, count: int, verbose: bool):
     ana = Anagram()
-
-    print("Le input doit être au format suivant")
-    letters, count = input("letters <space> count:\t").split()
-    if count.isalnum():
-        count = int(count)
-    else:
-        raise TypeError(f"count doit être un int et non {type(count)}")
-
     results = ana.process(letters, count)
+    if verbose:
+        pprint(results)
+    else:
+        pprint(results, stream=open("results.txt", "w"))
+        pprint("Les résultats ont été écrit dans './results.txt'")
 
-    pprint(results, stream=open("results.txt", "w"))
-    pprint("Les résultats ont été écrit dans './results.txt'")
+
+if __name__ == "__main__":
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("letters", help="Les lettres à rechercher", type=str)
+    parser.add_argument("count", help="Le nombre lettre", type=int)
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="Afficher les résultat sur la console",
+        action="store_true",
+    )
+    args = parser.parse_args()
+
+    main(args.letters, args.count, args.verbose)
